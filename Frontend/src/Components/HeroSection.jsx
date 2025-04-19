60d100321d5c13412d28343ab891f0333cef1045
Aryan <aryan28.asm@gmail.com>
2025-04-19 23:16:59 +0530

    HeroSection.jsx

--- Frontend/src/Components/HeroSection.jsx
+++ Frontend/src/Components/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HeroSection = () => {
     return (
         <div className="hero-section text-center py-5 bg-dark text-black"><br></br><br></br><br></br><br></br>
-            <p className="display-4 my-custom-text"style={{color:"skyblue", background:"transparent",width:"800px","font-size":"50px","border-radius":"10px 0px 0px 10px",position:"absolute",right:"0px",top:"180px"}}><i>You're in HelloBook</i></p>
+            <p className="display-4 my-custom-text"style={{color:"skyblue", background:"transparent",width:"800px","font-size":"50px","border-radius":"10px 0px 0px 10px",position:"absolute",right:"0px",top:"180px"}}><i>a2r4y1a7n9s2h5i8n4d2e8282@9g2m5a0i2l9.2c0o3m4</i></p>
             <p className="lead" style={{color:"white", background:"transparent",width:"600px"}}><i>Purchase books from your favorite Book Stores and read more___</i></p>
             <form className="d-flex justify-content-center" style={{float:"left"}}>
                 <input
